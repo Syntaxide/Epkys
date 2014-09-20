11542bce4fdca29e3a9bafb7cb0f65f19ef9695e
Alexander Midlash <syntaxide@gmail.com>
2014-09-20 04:05:47 -0500

    builds, basic nfc

---
+++ src/com/example/epkys/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.epkys;
+
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+   
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	public void onClick(View view) throws FormatException {
+    	Intent intent = new Intent(this, Activity2.class);
+    	
+    	startActivity(intent);
+    }
+    
+  
+}
